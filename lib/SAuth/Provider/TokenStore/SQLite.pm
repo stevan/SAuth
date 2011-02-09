657331fcb91dce782e1ed43a49b2a4f718cfaa29
Stevan Little <stevan.little@iinteractive.com>
2011-02-09 11:53:04 -0500

    implement the SQLite token store

---
+++ lib/SAuth/Provider/TokenStore/SQLite.pm
@@ -0,0 +1,82 @@
+package SAuth::Provider::TokenStore::SQLite;
+use Moose;
+
+use SAuth::Util;
+
+with 'SAuth::Provider::TokenStore',
+     'SAuth::Core::Role::WithSQLiteHandle';
+
+sub BUILD {
+    my $self = shift;
+    $self->dbh->do(q[
+        CREATE TABLE IF NOT EXISTS access_grants (
+            token        VARCHAR( 36 ),
+            access_grant TEXT,
+            nonce        VARCHAR( 36 ),
+            PRIMARY KEY ( token )
+        )
+    ]);
+}
+
+sub has_access_grant_for_token {
+    my ($self, $token) = @_;
+    my $sth = $self->dbh->prepare( 'SELECT COUNT(*) FROM access_grants WHERE token = ?' );
+    $sth->execute( $token );
+    my ($count) = $sth->fetchrow_array;
+    $count ? 1 : 0;
+}
+
+sub get_access_grant_for_token {
+    my ($self, $token) = @_;
+    my $sth = $self->dbh->prepare( 'SELECT access_grant FROM access_grants WHERE token = ?' );
+    $sth->execute( $token );
+    my ($access_grant_json) = $sth->fetchrow_array;
+    SAuth::Core::AccessGrant->from_json( $access_grant_json );
+}
+
+sub add_access_grant_for_token {
+    my ($self, $access_grant) = @_;
+    $self->dbh->do(
+        'INSERT INTO access_grants (token, access_grant, nonce) VALUES(?, ?, ?)',
+        {},
+        $access_grant->token,
+        $access_grant->to_json,
+        SAuth::Util::encode_base64( $access_grant->nonce )
+    );
+}
+
+sub get_current_nonce_for_token {
+    my ($self, $token) = @_;
+    my $sth = $self->dbh->prepare( 'SELECT nonce FROM access_grants WHERE token = ?' );
+    $sth->execute( $token );
+    my ($nonce) = $sth->fetchrow_array;
+    SAuth::Util::decode_base64( $nonce );
+}
+
+sub update_nonce_for_token {
+    my ($self, $token, $nonce) = @_;
+    my $access_grant = $self->get_access_grant_for_token( $token );
+    $access_grant->nonce( $nonce );
+    $self->dbh->do(
+        'UPDATE access_grants SET access_grant = ?, nonce = ? WHERE token = ?',
+        {},
+        $access_grant->to_json,
+        SAuth::Util::encode_base64( $nonce ),
+        $access_grant->token,
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+no Moose; 1;
+
+__END__
+
+# ABSTRACT: A Moosey solution to this problem
+
+=head1 SYNOPSIS
+
+  use SAuth::Provider::TokenStore::SQLite;
+
+=head1 DESCRIPTION
+
