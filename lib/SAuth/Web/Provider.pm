73a7687174c55ad4ffb18eb79a9386fdd6cde7cf
Stevan Little <stevan.little@iinteractive.com>
2011-02-11 15:13:10 -0500

    implement the refresh in the web provider

--- lib/SAuth/Web/Provider.pm
+++ lib/SAuth/Web/Provider.pm
@@ -27,47 +27,18 @@ sub build_router {
     $router->add_route(
         '/request_access',
         target => sub {
-            my $r = Plack::Request->new( shift );
+            return $self->process_request(
+                'process_access_request' => Plack::Request->new( shift )
+            );
+        }
+    );
 
-            return HTTP::Throwable::MethodNotAllowed->new(
-                allow   => [ 'POST' ],
-                message => 'Only POST is supported'
-            )->as_psgi
-                if $r->method ne 'POST';
-
-            # TODO:
-            # Support sending as JSON, as well as
-            # application/x-www-form-urlencoded,
-            # either way should be fine.
-            # - SL
-
-            my ($access_grant, $error);
-            try {
-                $access_grant = $self->provider->process_access_request(
-                    uid       => $r->param('uid'),
-                    hmac      => $r->param('hmac'),
-                    timestamp => int($r->param('timestamp')),
-                    body      => $r->param('body')
-                );
-            } catch {
-                $error = $_;
-            };
-
-            if ($error) {
-                return HTTP::Throwable::InternalServerError->new(
-                    message          => $error,
-                    show_stack_trace => 0
-                )->as_psgi;
-            }
-            else {
-                my $json = $access_grant->to_json;
-                return [
-                    200,
-                    [ 'Content-Type'   => 'application/json',
-                      'Content-Length' => length $json ],
-                    [ $json ]
-                ];
-            }
+    $router->add_route(
+        '/refresh_access',
+        target => sub {
+            return $self->process_request(
+                'process_access_refresh' => Plack::Request->new( shift )
+            );
         }
     );
 
@@ -85,6 +56,55 @@ sub build_router {
     );
 
     $router;
+}
+
+sub process_request {
+    my ($self, $provider_method, $r) = @_;
+
+    return HTTP::Throwable::MethodNotAllowed->new(
+        allow   => [ 'POST' ],
+        message => 'Only POST is supported'
+    )->as_psgi
+        if $r->method ne 'POST';
+
+    # TODO:
+    # Support sending as JSON, as well as
+    # application/x-www-form-urlencoded,
+    # either way should be fine.
+    # - SL
+
+    my ($access_grant, $error);
+    try {
+        $access_grant = $self->provider->$provider_method(
+            uid       => $r->param('uid'),
+            hmac      => $r->param('hmac'),
+            timestamp => int($r->param('timestamp')),
+            body      => $r->param('body')
+        );
+    } catch {
+        $error = $_;
+    };
+
+    return $self->process_result( $access_grant, $error );
+}
+
+sub process_result {
+    my ($self, $access_grant, $error) = @_;
+    if ($error) {
+        return HTTP::Throwable::InternalServerError->new(
+            message          => $error,
+            show_stack_trace => 0
+        )->as_psgi;
+    }
+    else {
+        my $json = $access_grant->to_json;
+        return [
+            200,
+            [ 'Content-Type'   => 'application/json',
+              'Content-Length' => length $json ],
+            [ $json ]
+        ];
+    }
 }
 
 
